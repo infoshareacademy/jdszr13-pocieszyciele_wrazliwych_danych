70ba4764df5e444022f1cd5c470c468ee909e892
Adrian Bednarczyk <adrianbednarczyk1@gmail.com>
2023-05-21 10:38:37 +0200

    dodatkowe dane

--- sql-projekt-pwd-/senat.sql
+++ sql-projekt-pwd-/senat.sql
@@ -34,11 +34,89 @@ where state = 'Alabama'
 group by candidate
 order by sum(total_votes) desc
 
-2315445
+2316445
 
-select sum(total_votes), 2315445 - sum(total_votes), 2315445 - sum(total_votes) + sum(total_votes)
+select sum(total_votes), 2316445 - sum(total_votes), 2316445 - sum(total_votes) + sum(total_votes)
 from senate_county_candidate scc 
 where state = 'Alabama'
-
+--Tommy 1,392,076
+--Jones 920,478
 2 31
 sdadsa
+
+
+
+
+
+-- tabela house
+select * 
+from house_state ss 
+select *
+from house_candidate scc 
+
+
+-- tabela president check danych
+select * 
+from president_state ss 
+where state = 'Delaware'
+
+select sum(total_votes) total, sum(current_votes) current
+from president_county sc 
+where state = 'Delaware'
+
+select *
+from president_county_candidate scc 
+where state = 'Delaware'
+
+select candidate, sum(total_votes)
+from president_county_candidate scc 
+where state = 'Delaware'
+group by candidate
+order by sum(total_votes) desc
+
+select  sum(total_votes)
+from president_county_candidate scc 
+where state = 'Delaware'
+
+
+-- tabela governors check danych
+select * 
+from governors_state ss 
+where state = 'Delaware'
+
+select sum(total_votes) total, sum(current_votes) current
+from governors_county sc 
+where state = 'Delaware'
+
+select *
+from governors_county_candidate scc 
+where state = 'Delaware'
+
+select candidate, sum(votes)
+from governors_county_candidate scc 
+where state = 'Delaware'
+group by candidate
+order by sum(votes) desc
+
+select  sum(votes)
+from governors_county_candidate scc 
+where state = 'Delaware'
+
+-- tabela governors 
+select * 
+from governors_county gc 
+
+select * 
+from governors_county_candidate gcc 
+
+select * 
+from governors_state gs 
+
+-- governors kto wygrał per county
+select * 
+	, case when 
+from governors_county_candidate gcc 
+where won = true
+
+select distinct(party)
+from governors_county_candidate gcc 
